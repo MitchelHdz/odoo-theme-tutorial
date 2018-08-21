24449bd9c6ec8ddb43a8de144ccc9aa60452516e
MitchelHdz <sebastianvicky_@hotmail.com>
2018-08-21 12:16:47 -0500

    [WIP] Working on snippets tutorial

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,14 @@
 {
     'name': "Theme tutorial",
-    'description': """A theme tutorial App on odoo 11"""
+    'description': """A theme tutorial App on odoo 11""",
     'version': '11.0',
-    'author': 'Ceviche'
+    'author': 'Ceviche',
     'data':[
+        'views/assets.xml',
+        'views/image_gallery.xml',
+        'views/snippets.xml',
+        'views/layout.xml',
+        'views/pages.xml',
     ],
     'category':'Theme/Creative',
     'depends':[
