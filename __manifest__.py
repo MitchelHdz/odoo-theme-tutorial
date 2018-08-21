3314f3a9c8c808149381940935755084875ab18e
MitchelHdz <sebastianvicky_@hotmail.com>
2018-08-21 09:55:42 -0500

    [ADD] First commit, adding manifest and init

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Theme tutorial",
+    'description': """A theme tutorial App on odoo 11"""
+    'version': '11.0',
+    'author': 'Ceviche'
+    'data':[
+    ],
+    'category':'Theme/Creative',
+    'depends':[
+        'website',
+    ],
+}
